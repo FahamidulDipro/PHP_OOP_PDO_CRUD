c5fbb1c21cf25a5172d078eba41eebc1beef66ed
FahamidulDipro <69914984+FahamidulDipro@users.noreply.github.com>
2021-01-23 00:48:37 +0600

    Use of Custom Class

--- index.php
+++ index.php
@@ -5,6 +5,24 @@
         public $lastname;
         public $fullname;
 
+        private $records=array();
+        
+        public function __set($name, $value)
+        {
+            $this->records[$name]=$value;
+        }
+
+        public function __get($name)
+        {
+        if(array_key_exists($name,$this->records)){
+            return  $this->records[$name];
+        }
+            
+        }
+        function toArray(){
+            return  $this->records;
+        }
+
         function __construct()
         {
             $this->fullname=$this->firstname.' '.$this->lastname;
@@ -17,8 +35,9 @@
     $stmt = $db->query("SELECT * FROM users");
     $stmt->setFetchMode(PDO::FETCH_CLASS,"Users");
     while($row = $stmt->fetch()){
-      
+        print_r($row->toArray());
         echo $row->fullname;
+
         // echo 'Firstname is :'.$row->firstname.' and lastname is: '.$row->lastname;
         echo'<br>';
            
