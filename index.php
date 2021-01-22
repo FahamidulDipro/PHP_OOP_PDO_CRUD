96eca01b6251f4c9abf6d8f329c624dfccf074f3
FahamidulDipro <69914984+FahamidulDipro@users.noreply.github.com>
2021-01-23 00:05:06 +0600

    Update index.php

--- index.php
+++ index.php
@@ -28,14 +28,3 @@
     // define("Numeric","Hellow! I am Numeric array constant");
     // echo Numeric;
 ?>
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP OOP PDO CRUD with Bootstrap</title>
-</head>
-<body>
-    <center><h1>PHP OOP PDO CRUD with Bootstrap</h1></center>
-</body>
-</html> -->
