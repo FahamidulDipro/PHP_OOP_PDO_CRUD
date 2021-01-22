c1a10db4adf737ecd0d7906868c886cabd21cf83
FahamidulDipro <69914984+FahamidulDipro@users.noreply.github.com>
2021-01-23 00:00:36 +0600

    Fetch Data in Class

--- index.php
+++ index.php
@@ -1,12 +1,28 @@
 <?php
     include "./include/db.php";
     class Users{
+        public $firstname;
+        public $lastname;
+        public $fullname;
 
+        function __construct()
+        {
+            $this->fullname=$this->firstname.' '.$this->lastname;
+
+        }
+        public function xyz(){
+            echo"Demo xyz";
+        }
     }
     $stmt = $db->query("SELECT * FROM users");
-    while($row = $stmt->fetch(PDO::FETCH_OBJ)){
-        echo $row->firstname;
+    $stmt->setFetchMode(PDO::FETCH_CLASS,"Users");
+    while($row = $stmt->fetch()){
+      
+        echo $row->fullname;
+        // echo 'Firstname is :'.$row->firstname.' and lastname is: '.$row->lastname;
         echo'<br>';
+           
+ 
     }
    
     // define("Numeric","Hellow! I am Numeric array constant");
