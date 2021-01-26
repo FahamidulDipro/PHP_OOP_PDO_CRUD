95c9400c48555f179de1a08b5cfc061d91498cae
FahamidulDipro <69914984+FahamidulDipro@users.noreply.github.com>
2021-01-26 23:19:34 +0600

    Adding Footer

--- edit.php
+++ edit.php
@@ -71,6 +71,9 @@ include "./include/db-config.php";
             </tr>
         </form>
     </div>
+    <?php
+        include "./include/footer.php";
+    ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </body>
---
+++ include/footer.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <div class="alert alert-primary text-center mt-3">
+    <strong>PHP CRUD <a href="#"></a></strong>
+
+    </div>
+
+</div>
